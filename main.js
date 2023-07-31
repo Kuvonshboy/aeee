6842d7be1518c59dcb79919b957abc7356faacef
kuvonshboy <bozorboyevquvonchbek0321@gmail.com>
2023-07-31 10:01:33 +0500

    43- uyga vazifa

---
+++ main.js
@@ -0,0 +1,97 @@
+// let url = `https://dog.ceo/api/breeds/image/random/3`;
+// let DoUrl = `https://dog.ceo/api/breeds/list/all`;
+
+// let imgurl = 'https://dog.ceo/api/breed/African/imges/random';
+
+
+// const img = document.getElementById(`img`);
+// const select = document.getElementById('select');
+
+// select.addEventListener('change', () =>{
+//     fetchApi(DoUrl)
+// })
+
+
+// function  fetchApi(url) {
+//     fetch(url)
+//         .then((res) => res.json())
+//         .then((data) => {
+//             let object = data.message;
+//             console.log(object);
+//             let objectOfArray = Object.keys(object);
+//             objectOfArray.map((item) =>{
+//                 const option = document.createElement('option');
+//                 option.value = item;
+//                 option.innerText = item;
+//                 select.appendChild(option);
+//             })   
+
+//     })
+// }
+    
+// fetchApi(DoUrl)
+    
+    
+// select.addEventListener(`change`, (e) => {
+//          let selectImg = 'https://dog.ceo/api/breed/${e.target.value}/imges/random';
+        
+
+//         fetch(selectImg)
+//          .then((res) => res.json())
+//          .then((data) =>{
+//             let newImg = data.message
+//             img.src =newImg
+//          })
+//     })
+
+
+
+// // fetchApi(url)
+
+
+// let url = `https://dog.ceo/api/breeds/image/random/3`;
+// function fetch1 (url) {
+//     fetch (url) 
+//     .then((res) => res.json())
+//     .then((data) => console.log(data))
+// }
+
+// fetch1(url)
+
+// let url = `https://dog.ceo/api/breeds/image/random/3`;
+// function fetch2 (url) {
+//     fetch (url) 
+//     .then((res) => res.json())
+//     .then((data) => console.log(data))
+// }
+
+
+// fetch2(url)
+
+// // let url = `https://dog.ceo/api/breeds/image/random/3`;
+// function fetch3 (url) {
+//     fetch (url) 
+//     .then((res) => res.json())
+//     .then((data) => console.log(data))
+// }
+
+
+// fetch3(url)
+
+//// let url = `https://dog.ceo/api/breeds/image/random/3`;
+// function fetch4 (url) {
+//     fetch (url) 
+//     .then((res) => res.json())
+//     .then((data) => console.log(data))
+// }
+
+// fetch4(url)
+
+// fetch5()// let url = `https://dog.ceo/api/breeds/image/random/3`;
+// function fetch1 (url) {
+//     fetch (url) 
+//     .then((res) => res.json())
+//     .then((data) => console.log(data))
+// }
+
+// fetch6()
